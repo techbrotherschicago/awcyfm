9b69ba9e8b0917ca59ad7ec87a9c7e662dc2fcde
techbrotherschicago <73196942+techbrotherschicago@users.noreply.github.com>
2026-03-27 16:03:13 -0500

    Update script.js

--- live_show/survey2/script.js
+++ live_show/survey2/script.js
@@ -1,1 +1,18 @@
+let popup = document.getElementById("popup");
+let checkbox = document.getElementById("checkbox");
+let turnred = document.getElementById("turnred");
 
+function unSelect() {
+  popup.classList.add("open-popup");
+  checkbox.checked = false;
+  turnred.classList.add("red-text")
+ 
+ 
+}
+ 
+function closePopup() {
+  popup.classList.remove("open-popup") 
+  
+}
+
+//setInterval('unSelect()', 200); 
